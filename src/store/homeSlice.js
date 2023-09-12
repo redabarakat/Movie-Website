004191bb6d86982b7ff15dac58f38b9e23d275ed
redabarakat <redabarakat65@gmail.com>
2023-09-12 17:14:15 +0300

    commit: fixed some error

--- src/store/homeSlice.js
+++ src/store/homeSlice.js
@@ -1,11 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchDataFromApi } from "../utils/api";
 
 const initialState = {
   url: {},
   genres: {},
-  loading: false,
-  error: null,
 };
 
 export const homeSlice = createSlice({
@@ -18,19 +15,6 @@ export const homeSlice = createSlice({
     getGenres: (state, action) => {
       state.genres = action.payload;
     },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(fetchDataFromApi.pending, (state) => {
-      state.loading = true;
-    });
-    builder.addCase(fetchDataFromApi.fulfilled, (state, action) => {
-      state.loading = false;
-      // state.url = action.payload;
-    });
-    builder.addCase(fetchDataFromApi.rejected, (state, action) => {
-      state.loading = false;
-      state.error = action.payload;
-    });
   },
 });
 
